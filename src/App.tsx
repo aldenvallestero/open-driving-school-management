539741dd6ca57fd60e862af84aa41f36674f0eae
Alden Vallestero <alden.v@agtuary.com>
2024-01-29 11:55:43 +0800

    ✨ Add Profile Page Route

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 const IdPage = React.lazy(() => import('./pages/id-page'))
 const LoginPage = React.lazy(() => import('./pages/login-page'))
+const ProfilePage = React.lazy(() => import('./pages/profile-page'))
 
 function App() {
   return (
@@ -14,6 +15,14 @@ function App() {
           element={
             <Suspense fallback="Login ...">
               <LoginPage />
+            </Suspense>
+          }
+          />
+        <Route
+          path='/profile' 
+          element={
+            <Suspense fallback="Profile ...">
+              <ProfilePage />
             </Suspense>
           }
         />
