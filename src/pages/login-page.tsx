8237a170d091bf67d109b022676e59502145375d
Alden Vallestero <alden.v@agtuary.com>
2024-02-03 14:49:27 +0800

    ⚰️ Remove unwanted codes

--- src/pages/login-page.tsx
+++ src/pages/login-page.tsx
@@ -4,10 +4,11 @@ import { useState } from 'react'
 
 export default function LoginPage() {
 
-  const [email, setEmail] = useState<string>();
-  const [password, setPassword] = useState<string>();
+  const [, setEmail] = useState<string>();
+  const [, setPassword] = useState<string>();
 
   const handleLogin = () => {
+    
     window.location.replace('/profile')
   }
 
