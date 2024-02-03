81f170b244d416f5c11f7988f1651025d05799fe
Alden Vallestero <alden.v@agtuary.com>
2024-02-03 14:18:35 +0800

    💄 Add ID Label

--- src/pages/id-page.tsx
+++ src/pages/id-page.tsx
@@ -1,7 +1,7 @@
 
 import QRCode from 'react-qr-code'
 import pic from '../media/pic-media.jpeg'
-import { useRef, useState } from 'react'
+import { useState } from 'react'
 
 export default function IdPage() {
 
@@ -14,6 +14,7 @@ export default function IdPage() {
       <div className="block bg-red-800 p-20">
         <h1 className="font-bold text-4xl">A1C Driving School</h1>
         <h1>Tumana, Sta. Maria, 3022, Bulacan</h1>
+        <h1>School ID</h1>
         <img className="mx-auto my-4" src={pic} alt="" />
         <h1 className="font-bold">{name}</h1>
         <small>{id}</small>
