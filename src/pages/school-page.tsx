5621c53cd9b7c20142282918c5fa617e1a8273b5
aldenvallestero <betas-pupa0p@icloud.com>
2024-06-25 16:09:30 +0800

    🚨 Fix eslint warnings

--- src/pages/school-page.tsx
+++ src/pages/school-page.tsx
@@ -248,7 +248,7 @@ export default function SchoolPage() {
 
   return (
     <div className="container-fluid bg-slate-200 h-screen p-6">
-      <Tabs aria-label="Default tabs" style={"default"}>
+      <Tabs aria-label="Default tabs">
         <Tabs.Item
           disabled={!branches || !courses || !students}
           title="Attendance"
